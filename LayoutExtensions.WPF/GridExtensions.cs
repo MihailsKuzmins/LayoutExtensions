fff87e3f39c5f86c9820ae0237b333610700f7b2
mihails.kuzmins <mihails.kuzmins01@gmail.com>
2019-07-06 15:32:18 +0300

    added GridExtensions

---
+++ LayoutExtensions.WPF/GridExtensions.cs
@@ -0,0 +1,22 @@
+﻿using JetBrains.Annotations;
+using LayoutExtensions.WPF.Extensions;
+using System.Collections.Generic;
+using System.Windows.Controls;
+
+namespace LayoutExtensions.WPF
+{
+	public static class GridExtensions
+	{
+		public static Grid Rows([NotNull] this Grid @this, IEnumerable<RowDefinition> rows)
+		{
+			@this.RowDefinitions.AddRange(rows);
+			return @this;
+		}
+
+		public static Grid Cols([NotNull] this Grid @this, IEnumerable<ColumnDefinition> cols)
+		{
+			@this.ColumnDefinitions.AddRange(cols);
+			return @this;
+		}
+	}
+}
