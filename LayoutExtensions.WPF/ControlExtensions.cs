fbb08e1e6c2d88fb4f989d694a5489dc22e7dd91
MihailsKuzmins <47413092+MihailsKuzmins@users.noreply.github.com>
2019-07-06 15:39:10 +0300

    Initial commit (#2)
    * added FrameworkElementExtensions
    
    * created ControlExtensions
    
    * additional for margin
    
    * additional extensions for aligments
    
    * created ContentControlExtensions
    
    * added assign extension
    
    * created ContextMenuExtensions
    
    * extensions for panels
    
    * added GridExtensions
    
    * Create LICENSE
    
    * assembly info

---
+++ LayoutExtensions.WPF/ControlExtensions.cs
@@ -0,0 +1,402 @@
+﻿using JetBrains.Annotations;
+using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using H = System.Windows.HorizontalAlignment;
+using V = System.Windows.VerticalAlignment;
+
+namespace LayoutExtensions.WPF
+{
+	public static class ControlExtensions
+	{
+		public static TControl FontStyle<TControl>([NotNull] this TControl @this, FontStyle fontStyle)
+			where TControl : Control
+		{
+			@this.FontStyle = fontStyle;
+			return @this;
+		}
+
+		public static TControl FontStretch<TControl>([NotNull] this TControl @this, FontStretch fontStretch)
+			where TControl : Control
+		{
+			@this.FontStretch = fontStretch;
+			return @this;
+		}
+
+		public static TControl FontSize<TControl>([NotNull] this TControl @this, double fontSize)
+			where TControl : Control
+		{
+			@this.FontSize = fontSize;
+			return @this;
+		}
+
+		public static TControl FontFamily<TControl>([NotNull] this TControl @this, FontFamily fontFamily)
+			where TControl : Control
+		{
+			@this.FontFamily = fontFamily;
+			return @this;
+		}
+
+		public static TControl FontFamily<TControl>([NotNull] this TControl @this, string fontFamily)
+			where TControl : Control
+		{
+			@this.FontFamily = new FontFamily(fontFamily);
+			return @this;
+		}
+
+		public static TControl FontFamily<TControl>([NotNull] this TControl @this, Uri uri, string fontFamily)
+			where TControl : Control
+		{
+			@this.FontFamily = new FontFamily(uri, fontFamily);
+			return @this;
+		}
+
+		public static TControl FontFamily<TControl>([NotNull] this TControl @this, FontSizes fontSize)
+			where TControl : Control
+		{
+			@this.FontFamily = new FontFamily(fontSize.ToFontSizeString());
+			return @this;
+		}
+
+		public static TControl Background<TControl>([NotNull] this TControl @this, Brush brush)
+			where TControl : Control
+		{
+			@this.Background = brush;
+			return @this;
+		}
+
+		public static TControl Background<TControl>([NotNull] this TControl @this, Color color)
+			where TControl : Control
+		{
+			@this.Background = new SolidColorBrush(color);
+			return @this;
+		}
+
+		public static TControl Background<TControl>([NotNull] this TControl @this, byte a, byte r, byte g, byte b)
+			where TControl : Control
+		{
+			@this.Background = new SolidColorBrush(Color.FromArgb(a, r, g, b));
+			return @this;
+		}
+
+		public static TControl Background<TControl>([NotNull] this TControl @this, byte r, byte g, byte b)
+			where TControl : Control
+		{
+			@this.Background = new SolidColorBrush(Color.FromRgb(r, g, b));
+			return @this;
+		}
+
+		public static TControl Foreground<TControl>([NotNull] this TControl @this, Brush brush)
+			where TControl : Control
+		{
+			@this.Foreground = brush;
+			return @this;
+		}
+
+		public static TControl Foreground<TControl>([NotNull] this TControl @this, Color color)
+			where TControl : Control
+		{
+			@this.Foreground = new SolidColorBrush(color);
+			return @this;
+		}
+
+		public static TControl Foreground<TControl>([NotNull] this TControl @this, byte a, byte r, byte g, byte b)
+			where TControl : Control
+		{
+			@this.Foreground = new SolidColorBrush(Color.FromArgb(a, r, g, b));
+			return @this;
+		}
+
+		public static TControl Foreground<TControl>([NotNull] this TControl @this, byte r, byte g, byte b)
+			where TControl : Control
+		{
+			@this.Foreground = new SolidColorBrush(Color.FromRgb(r, g, b));
+			return @this;
+		}
+
+		public static TControl Border<TControl>([NotNull] this TControl @this, double uniform)
+			where TControl : Control
+		{
+			@this.BorderThickness = new Thickness(uniform);
+			return @this;
+		}
+
+		public static TControl Border<TControl>([NotNull] this TControl @this, double horizontal, double vertical)
+			where TControl : Control
+		{
+			@this.BorderThickness = new Thickness(horizontal, vertical, horizontal, vertical);
+			return @this;
+		}
+
+		public static TControl Border<TControl>([NotNull] this TControl @this, double left, double top, double right, double bottom)
+			where TControl : Control
+		{
+			@this.BorderThickness = new Thickness(left, top, right, bottom);
+			return @this;
+		}
+
+		public static TControl BorderHorizontal<TControl>([NotNull] this TControl @this, double horizontal)
+			where TControl : Control
+		{
+			var border = @this.BorderThickness;
+
+			@this.BorderThickness = new Thickness(horizontal, border.Top, horizontal, border.Bottom);
+			return @this;
+		}
+
+		public static TControl BorderVertical<TControl>([NotNull] this TControl @this, double vertical)
+			where TControl : Control
+		{
+			var border = @this.BorderThickness;
+
+			@this.BorderThickness = new Thickness(border.Left, vertical, border.Right, vertical);
+			return @this;
+		}
+
+		public static TControl BorderLeft<TControl>([NotNull] this TControl @this, double left)
+			where TControl : Control
+		{
+			var border = @this.BorderThickness;
+
+			@this.BorderThickness = new Thickness(left, border.Top, border.Right, border.Bottom);
+			return @this;
+		}
+
+		public static TControl BorderTop<TControl>([NotNull] this TControl @this, double top)
+			where TControl : Control
+		{
+			var border = @this.BorderThickness;
+
+			@this.BorderThickness = new Thickness(border.Left, top, border.Right, border.Bottom);
+			return @this;
+		}
+
+		public static TControl BorderRight<TControl>([NotNull] this TControl @this, double right)
+			where TControl : Control
+		{
+			var border = @this.BorderThickness;
+
+			@this.BorderThickness = new Thickness(border.Left, border.Top, right, border.Bottom);
+			return @this;
+		}
+
+		public static TControl BorderBottom<TControl>([NotNull] this TControl @this, double bottom)
+			where TControl : Control
+		{
+			var border = @this.BorderThickness;
+
+			@this.BorderThickness = new Thickness(border.Left, border.Top, border.Right, bottom);
+			return @this;
+		}
+
+		public static TControl IsTabStop<TControl>([NotNull] this TControl @this, bool isTabStop)
+			where TControl : Control
+		{
+			@this.IsTabStop = isTabStop;
+			return @this;
+		}
+
+		public static TControl VerticalContentAlignment<TControl>([NotNull] this TControl @this, VerticalAlignment verticalAlignment)
+			where TControl : Control
+		{
+			@this.VerticalContentAlignment = verticalAlignment;
+			return @this;
+		}
+
+		public static TControl TabIndex<TControl>([NotNull] this TControl @this, int tabIndex)
+			where TControl : Control
+		{
+			@this.TabIndex = tabIndex;
+			return @this;
+		}
+
+		public static TControl Padding<TControl>([NotNull] this TControl @this, double uniform)
+			where TControl : Control
+		{
+			@this.Padding = new Thickness(uniform);
+			return @this;
+		}
+
+		public static TControl Padding<TControl>([NotNull] this TControl @this, double horizontal, double vertical)
+			where TControl : Control
+		{
+			@this.Padding = new Thickness(horizontal, vertical, horizontal, vertical);
+			return @this;
+		}
+
+		public static TControl Padding<TControl>([NotNull] this TControl @this, double left, double top, double right, double bottom)
+			where TControl : Control
+		{
+			@this.Padding = new Thickness(left, top, right, bottom);
+			return @this;
+		}
+
+		public static TControl PaddingHorizontal<TControl>([NotNull] this TControl @this, double horizontal)
+			where TControl : Control
+		{
+			var Padding = @this.Padding;
+
+			@this.Padding = new Thickness(horizontal, Padding.Top, horizontal, Padding.Bottom);
+			return @this;
+		}
+
+		public static TControl PaddingVertical<TControl>([NotNull] this TControl @this, double vertical)
+			where TControl : Control
+		{
+			var Padding = @this.Padding;
+
+			@this.Padding = new Thickness(Padding.Left, vertical, Padding.Right, vertical);
+			return @this;
+		}
+
+		public static TControl PaddingLeft<TControl>([NotNull] this TControl @this, double left)
+			where TControl : Control
+		{
+			var Padding = @this.Padding;
+
+			@this.Padding = new Thickness(left, Padding.Top, Padding.Right, Padding.Bottom);
+			return @this;
+		}
+
+		public static TControl PaddingTop<TControl>([NotNull] this TControl @this, double top)
+			where TControl : Control
+		{
+			var Padding = @this.Padding;
+
+			@this.Padding = new Thickness(Padding.Left, top, Padding.Right, Padding.Bottom);
+			return @this;
+		}
+
+		public static TControl PaddingRight<TControl>([NotNull] this TControl @this, double right)
+			where TControl : Control
+		{
+			var Padding = @this.Padding;
+
+			@this.Padding = new Thickness(Padding.Left, Padding.Top, right, Padding.Bottom);
+			return @this;
+		}
+
+		public static TControl PaddingBottom<TControl>([NotNull] this TControl @this, double bottom)
+			where TControl : Control
+		{
+			var Padding = @this.Padding;
+
+			@this.Padding = new Thickness(Padding.Left, Padding.Top, Padding.Right, bottom);
+			return @this;
+		}
+
+		public static TControl Template<TControl>([NotNull] this TControl @this, ControlTemplate template)
+			where TControl : Control
+		{
+			@this.Template = template;
+			return @this;
+		}
+
+		public static TControl FontWeight<TControl>([NotNull] this TControl @this, FontWeight fontWeight)
+			where TControl : Control
+		{
+			@this.FontWeight = fontWeight;
+			return @this;
+		}
+
+		public static TControl BorderBrush<TControl>([NotNull] this TControl @this, Brush brush)
+			where TControl : Control
+		{
+			@this.BorderBrush = brush;
+			return @this;
+		}
+
+		public static TControl BorderBrush<TControl>([NotNull] this TControl @this, Color color)
+			where TControl : Control
+		{
+			@this.BorderBrush = new SolidColorBrush(color);
+			return @this;
+		}
+
+		public static TControl BorderBrush<TControl>([NotNull] this TControl @this, byte a, byte r, byte g, byte b)
+			where TControl : Control
+		{
+			@this.BorderBrush = new SolidColorBrush(Color.FromArgb(a, r, g, b));
+			return @this;
+		}
+
+		public static TControl BorderBrush<TControl>([NotNull] this TControl @this, byte r, byte g, byte b)
+			where TControl : Control
+		{
+			@this.BorderBrush = new SolidColorBrush(Color.FromRgb(r, g, b));
+			return @this;
+		}
+
+		public static TControl HorizontalContentAlignment<TControl>([NotNull] this TControl @this, HorizontalAlignment horizontalAlignment)
+			where TControl : Control
+		{
+			@this.HorizontalContentAlignment = horizontalAlignment;
+			return @this;
+		}
+
+		public static TControl ContentTop<TControl>([NotNull] this TControl @this)
+			where TControl : Control
+		{
+			return @this.VerticalContentAlignment(V.Top);
+		}
+
+		public static TControl ContentCenterV<TControl>([NotNull] this TControl @this)
+			where TControl : Control
+		{
+			return @this.VerticalContentAlignment(V.Center);
+		}
+
+		public static TControl ContentCenterH<TControl>([NotNull] this TControl @this)
+			where TControl : Control
+		{
+			return @this.HorizontalContentAlignment(H.Center);
+		}
+
+		public static TControl ContentCenter<TControl>([NotNull] this TControl @this)
+			where TControl : Control
+		{
+			return @this
+				.ContentCenterH()
+				.ContentCenterV();
+		}
+
+		public static TControl ContentBotton<TControl>([NotNull] this TControl @this)
+			where TControl : Control
+		{
+			return @this.VerticalContentAlignment(V.Bottom);
+		}
+
+		public static TControl ContentStretchV<TControl>([NotNull] this TControl @this)
+			where TControl : Control
+		{
+			return @this.VerticalContentAlignment(V.Stretch);
+		}
+
+		public static TControl ContentStretchH<TControl>([NotNull] this TControl @this)
+			where TControl : Control
+		{
+			return @this.HorizontalContentAlignment(H.Stretch);
+		}
+
+		public static TControl ContentStretch<TControl>([NotNull] this TControl @this)
+			where TControl : Control
+		{
+			return @this
+				.ContentStretchH()
+				.ContentStretchV();
+		}
+
+		public static TControl ContentLeft<TControl>([NotNull] this TControl @this)
+			where TControl : Control
+		{
+			return @this.HorizontalContentAlignment(H.Left);
+		}
+
+		public static TControl ContentRight<TControl>([NotNull] this TControl @this)
+			where TControl : Control
+		{
+			return @this.HorizontalContentAlignment(H.Right);
+		}
+	}
+}
---
+++ LayoutExtensions.WPF/FrameworkElementExtensions.cs
@@ -0,0 +1,333 @@
+﻿using JetBrains.Annotations;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using H = System.Windows.HorizontalAlignment;
+using V = System.Windows.VerticalAlignment;
+
+namespace LayoutExtensions.WPF
+{
+	public static class FrameworkElementExtensions
+	{
+		public static void Assign<TFrameworkElement>([NotNull] this TFrameworkElement @this, out TFrameworkElement frameworkElement)
+			where TFrameworkElement : FrameworkElement
+		{
+			frameworkElement = @this;
+		}
+
+		public static TFrameworkElement Width<TFrameworkElement>([NotNull] this TFrameworkElement @this, double width)
+			where TFrameworkElement : FrameworkElement
+		{
+			@this.Width = width;
+			return @this;
+		}
+
+		public static TFrameworkElement MinWidth<TFrameworkElement>([NotNull] this TFrameworkElement @this, double minWidth)
+			where TFrameworkElement : FrameworkElement
+		{
+			@this.MinWidth = minWidth;
+			return @this;
+		}
+
+		public static TFrameworkElement MaxHeight<TFrameworkElement>([NotNull] this TFrameworkElement @this, double maxHeight)
+			where TFrameworkElement : FrameworkElement
+		{
+			@this.MaxHeight = maxHeight;
+			return @this;
+		}
+
+		public static TFrameworkElement Height<TFrameworkElement>([NotNull] this TFrameworkElement @this, double height)
+			where TFrameworkElement : FrameworkElement
+		{
+			@this.Height = height;
+			return @this;
+		}
+
+		public static TFrameworkElement MinHeight<TFrameworkElement>([NotNull] this TFrameworkElement @this, double minHeight)
+			where TFrameworkElement : FrameworkElement
+		{
+			@this.MinHeight = minHeight;
+			return @this;
+		}
+
+		public static TFrameworkElement MaxWidth<TFrameworkElement>([NotNull] this TFrameworkElement @this, double maxWidth)
+			where TFrameworkElement : FrameworkElement
+		{
+			@this.MaxWidth = maxWidth;
+			return @this;
+		}
+
+		public static TFrameworkElement Tag<TFrameworkElement>([NotNull] this TFrameworkElement @this, object tag)
+			where TFrameworkElement : FrameworkElement
+		{
+			@this.Tag = tag;
+			return @this;
+		}
+
+		public static T GetTag<TFrameworkElement, T>([NotNull] this TFrameworkElement @this)
+			where TFrameworkElement : FrameworkElement
+		{
+			return (T)@this.Tag;
+		}
+
+		public static TFrameworkElement Name<TFrameworkElement>([NotNull] this TFrameworkElement @this, string name)
+			where TFrameworkElement : FrameworkElement
+		{
+			@this.Name = name;
+			return @this;
+		}
+
+		public static TFrameworkElement DataContext<TFrameworkElement>([NotNull] this TFrameworkElement @this, object dataContext)
+			where TFrameworkElement : FrameworkElement
+		{
+			@this.DataContext = dataContext;
+			return @this;
+		}
+
+		public static T GetDataContext<TFrameworkElement, T>([NotNull] this TFrameworkElement @this)
+			where TFrameworkElement : FrameworkElement
+		{
+			return (T)@this.DataContext;
+		}
+
+		public static TFrameworkElement UseLayoutRounding<TFrameworkElement>([NotNull] this TFrameworkElement @this, bool useLayoutRouting)
+			where TFrameworkElement : FrameworkElement
+		{
+			@this.UseLayoutRounding = useLayoutRouting;
+			return @this;
+		}
+
+		public static TFrameworkElement FlowDirection<TFrameworkElement>([NotNull] this TFrameworkElement @this, FlowDirection flowDirection)
+			where TFrameworkElement : FrameworkElement
+		{
+			@this.FlowDirection = flowDirection;
+			return @this;
+		}
+
+		public static TFrameworkElement Margin<TFrameworkElement>([NotNull] this TFrameworkElement @this, double uniform)
+			where TFrameworkElement : FrameworkElement
+		{
+			@this.Margin = new Thickness(uniform);
+			return @this;
+		}
+
+		public static TFrameworkElement Margin<TFrameworkElement>([NotNull] this TFrameworkElement @this, double horizontal, double vertical)
+			where TFrameworkElement : FrameworkElement
+		{
+			@this.Margin = new Thickness(horizontal, vertical, horizontal, vertical);
+			return @this;
+		}
+
+		public static TFrameworkElement Margin<TFrameworkElement>([NotNull] this TFrameworkElement @this, double left, double top, double right, double bottom)
+			where TFrameworkElement : FrameworkElement
+		{
+			@this.Margin = new Thickness(left, top, right, bottom);
+			return @this;
+		}
+
+		public static TFrameworkElement MarginHorizontal<TFrameworkElement>([NotNull] this TFrameworkElement @this, double horizontal)
+			where TFrameworkElement : FrameworkElement
+		{
+			var margin = @this.Margin;
+
+			@this.Margin = new Thickness(horizontal, margin.Top, horizontal, margin.Bottom);
+			return @this;
+		}
+
+		public static TFrameworkElement MarginVertical<TFrameworkElement>([NotNull] this TFrameworkElement @this, double vertical)
+			where TFrameworkElement : FrameworkElement
+		{
+			var margin = @this.Margin;
+
+			@this.Margin = new Thickness(margin.Left, vertical, margin.Right, vertical);
+			return @this;
+		}
+
+		public static TFrameworkElement MarginLeft<TFrameworkElement>([NotNull] this TFrameworkElement @this, double left)
+			where TFrameworkElement : FrameworkElement
+		{
+			var margin = @this.Margin;
+
+			@this.Margin = new Thickness(left, margin.Top, margin.Right, margin.Bottom);
+			return @this;
+		}
+
+		public static TFrameworkElement MarginTop<TFrameworkElement>([NotNull] this TFrameworkElement @this, double top)
+			where TFrameworkElement : FrameworkElement
+		{
+			var margin = @this.Margin;
+
+			@this.Margin = new Thickness(margin.Left, top, margin.Right, margin.Bottom);
+			return @this;
+		}
+
+		public static TFrameworkElement MarginRight<TFrameworkElement>([NotNull] this TFrameworkElement @this, double right)
+			where TFrameworkElement : FrameworkElement
+		{
+			var margin = @this.Margin;
+
+			@this.Margin = new Thickness(margin.Left, margin.Top, right, margin.Bottom);
+			return @this;
+		}
+
+		public static TFrameworkElement MarginBottom<TFrameworkElement>([NotNull] this TFrameworkElement @this, double bottom)
+			where TFrameworkElement : FrameworkElement
+		{
+			var margin = @this.Margin;
+
+			@this.Margin = new Thickness(margin.Left, margin.Top, margin.Right, bottom);
+			return @this;
+		}
+
+		public static TFrameworkElement Style<TFrameworkElement>([NotNull] this TFrameworkElement @this, Style style)
+			where TFrameworkElement : FrameworkElement
+		{
+			@this.Style = style;
+			return @this;
+		}
+
+		public static TFrameworkElement VerticalAlignment<TFrameworkElement>([NotNull] this TFrameworkElement @this, VerticalAlignment verticalAlignment)
+			where TFrameworkElement : FrameworkElement
+		{
+			@this.VerticalAlignment = verticalAlignment;
+			return @this;
+		}
+
+		public static TFrameworkElement OverridesDefaultStyle<TFrameworkElement>([NotNull] this TFrameworkElement @this, bool overridesDefaultStyle)
+			where TFrameworkElement : FrameworkElement
+		{
+			@this.OverridesDefaultStyle = overridesDefaultStyle;
+			return @this;
+		}
+
+		public static TFrameworkElement HorizontalAlignment<TFrameworkElement>([NotNull] this TFrameworkElement @this, HorizontalAlignment horizontalAlignment)
+			where TFrameworkElement : FrameworkElement
+		{
+			@this.HorizontalAlignment = horizontalAlignment;
+			return @this;
+		}
+
+		public static TFrameworkElement ContextMenu<TFrameworkElement>([NotNull] this TFrameworkElement @this, ContextMenu contextMenu)
+			where TFrameworkElement : FrameworkElement
+		{
+			@this.ContextMenu = contextMenu;
+			return @this;
+		}
+
+		public static TFrameworkElement ToolTip<TFrameworkElement>([NotNull] this TFrameworkElement @this, object toolTip)
+			where TFrameworkElement : FrameworkElement
+		{
+			@this.ToolTip = toolTip;
+			return @this;
+		}
+
+		public static TFrameworkElement ForceCursor<TFrameworkElement>([NotNull] this TFrameworkElement @this, bool forceCursor)
+			where TFrameworkElement : FrameworkElement
+		{
+			@this.ForceCursor = forceCursor;
+			return @this;
+		}
+
+		public static void Cursor<TFrameworkElement>([NotNull] this TFrameworkElement @this, Cursor cursor)
+			where TFrameworkElement : FrameworkElement
+		{
+			@this.Cursor = cursor;
+		}
+
+		public static TFrameworkElement FocusVisualStyle<TFrameworkElement>([NotNull] this TFrameworkElement @this, Style focusVisualStyle)
+			where TFrameworkElement : FrameworkElement
+		{
+			@this.FocusVisualStyle = focusVisualStyle;
+			return @this;
+		}
+
+		public static TFrameworkElement Top<TFrameworkElement>([NotNull] this TFrameworkElement @this)
+			where TFrameworkElement : FrameworkElement
+		{
+			return @this.VerticalAlignment(V.Top);
+		}
+
+		public static TFrameworkElement CenterV<TFrameworkElement>([NotNull] this TFrameworkElement @this)
+			where TFrameworkElement : FrameworkElement
+		{
+			return @this.VerticalAlignment(V.Center);
+		}
+
+		public static TFrameworkElement CenterH<TFrameworkElement>([NotNull] this TFrameworkElement @this)
+			where TFrameworkElement : FrameworkElement
+		{
+			return @this.HorizontalAlignment(H.Center);
+		}
+
+		public static TFrameworkElement Center<TFrameworkElement>([NotNull] this TFrameworkElement @this)
+			where TFrameworkElement : FrameworkElement
+		{
+			return @this
+				.CenterH()
+				.CenterV();
+		}
+
+		public static TFrameworkElement Botton<TFrameworkElement>([NotNull] this TFrameworkElement @this)
+			where TFrameworkElement : FrameworkElement
+		{
+			return @this.VerticalAlignment(V.Bottom);
+		}
+
+		public static TFrameworkElement StretchV<TFrameworkElement>([NotNull] this TFrameworkElement @this)
+			where TFrameworkElement : FrameworkElement
+		{
+			return @this.VerticalAlignment(V.Stretch);
+		}
+
+		public static TFrameworkElement StretchH<TFrameworkElement>([NotNull] this TFrameworkElement @this)
+			where TFrameworkElement : FrameworkElement
+		{
+			return @this.HorizontalAlignment(H.Stretch);
+		}
+
+		public static TFrameworkElement Stretch<TFrameworkElement>([NotNull] this TFrameworkElement @this)
+			where TFrameworkElement : FrameworkElement
+		{
+			return @this
+				.StretchH()
+				.StretchV();
+		}
+
+		public static TFrameworkElement Left<TFrameworkElement>([NotNull] this TFrameworkElement @this)
+			where TFrameworkElement : FrameworkElement
+		{
+			return @this.HorizontalAlignment(H.Left);
+		}
+
+		public static TFrameworkElement Right<TFrameworkElement>([NotNull] this TFrameworkElement @this)
+			where TFrameworkElement : FrameworkElement
+		{
+			return @this.HorizontalAlignment(H.Right);
+		}
+
+		public static TFrameworkElement AddToPanel<TFrameworkElement>([NotNull] this TFrameworkElement @this, Panel panel)
+			where TFrameworkElement : FrameworkElement
+		{
+			panel.Children.Add(@this);
+			return @this;
+		}
+
+		public static TFrameworkElement Row<TFrameworkElement>([NotNull] this TFrameworkElement @this, int row, int rowSpan = 1)
+			where TFrameworkElement : FrameworkElement
+		{
+			Grid.SetRow(@this, row);
+			Grid.SetRowSpan(@this, rowSpan);
+
+			return @this;
+		}
+
+		public static TFrameworkElement Col<TFrameworkElement>([NotNull] this TFrameworkElement @this, int column, int colSpan = 1)
+			where TFrameworkElement : FrameworkElement
+		{
+			Grid.SetColumn(@this, column);
+			Grid.SetColumnSpan(@this, colSpan);
+
+			return @this;
+		}
+	}
+}
