2d8f8384c2ac32ed36a1b0f22d8978bb0d51e78d
MihailsKuzmins <47413092+MihailsKuzmins@users.noreply.github.com>
2019-07-06 22:37:57 +0300

    style extensions (#5)
    * update iconUrl
    
    * changed Dependencies
    
    * updated an icon
    
    * assembly info
    
    * added Style extensions

---
+++ LayoutExtensions.WPF/StyleExtensions.cs
@@ -0,0 +1,15 @@
+﻿using JetBrains.Annotations;
+using System.Windows;
+using System.Windows.Data;
+
+namespace LayoutExtensions.WPF
+{
+	public static class StyleExtensions
+	{
+		public static Style Setter([NotNull] this Style @this, DependencyProperty dependencyProperty, Binding binding)
+		{
+			@this.Setters.Add(new Setter(dependencyProperty, binding));
+			return @this;
+		}
+	}
+}
