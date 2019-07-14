fcbf7b17b093d8e7c521c79c74eb28701c4f8125
mihails.kuzmins <mihails.kuzmins01@gmail.com>
2019-07-14 13:52:12 +0300

    StackPanelExtensions

---
+++ LayoutExtensions.WPF/StackPanelExtensions.cs
@@ -0,0 +1,21 @@
+﻿using JetBrains.Annotations;
+using System.Windows.Controls;
+using O = System.Windows.Controls.Orientation;
+
+namespace LayoutExtensions.WPF
+{
+	public static class StackPanelExtensions
+	{
+		public static StackPanel Horizontal([NotNull] this StackPanel @this) =>
+			@this.Orientation(O.Horizontal);
+
+		public static StackPanel Vertical([NotNull] this StackPanel @this) =>
+			@this.Orientation(O.Vertical);
+
+		private static StackPanel Orientation([NotNull] this StackPanel @this, O orientation)
+		{
+			@this.Orientation = orientation;
+			return @this;
+		}
+	}
+}
