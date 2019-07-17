528739aba305ee823280b557f9d00f03048a40a1
mihails.kuzmins <mihails.kuzmins01@gmail.com>
2019-07-17 21:58:55 +0300

    TextBlockExtensions

---
+++ LayoutExtensions.WPF/src/TextBlockExtensions.cs
@@ -0,0 +1,39 @@
+﻿using JetBrains.Annotations;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace LayoutExtensions.WPF.src
+{
+	public static class TextBlockExtensions
+	{
+		public static TextBlock Foreground([NotNull] this TextBlock @this, Brush brush)
+		{
+			@this.Foreground = brush;
+			return @this;
+		}
+
+		public static TextBlock Foreground([NotNull] this TextBlock @this, Color color)
+		{
+			@this.Foreground = new SolidColorBrush(color);
+			return @this;
+		}
+
+		public static TextBlock Foreground([NotNull] this TextBlock @this, byte a, byte r, byte g, byte b)
+		{
+			@this.Foreground = new SolidColorBrush(Color.FromArgb(a, r, g, b));
+			return @this;
+		}
+
+		public static TextBlock Foreground([NotNull] this TextBlock @this, byte r, byte g, byte b)
+		{
+			@this.Foreground = new SolidColorBrush(Color.FromRgb(r, g, b));
+			return @this;
+		}
+
+		public static TextBlock FontSize([NotNull] this TextBlock @this, double fontSize)
+		{
+			@this.FontSize = fontSize;
+			return @this;
+		}
+	}
+}
