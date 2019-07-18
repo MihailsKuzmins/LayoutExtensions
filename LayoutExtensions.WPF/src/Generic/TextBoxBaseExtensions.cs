93db025b2145da3e40c1fd92947fe2ebea41ca54
mihails.kuzmins <mihails.kuzmins01@gmail.com>
2019-07-18 21:08:49 +0300

    TextBoxBaseExtensions

---
+++ LayoutExtensions.WPF/src/Generic/TextBoxBaseExtensions.cs
@@ -0,0 +1,15 @@
+﻿using JetBrains.Annotations;
+using System.Windows.Controls.Primitives;
+
+namespace LayoutExtensions.WPF
+{
+	public static class TextBoxBaseExtensions
+	{
+		public static TTextBoxBase ReadOnly<TTextBoxBase>([NotNull] this TTextBoxBase @this)
+			where TTextBoxBase : TextBoxBase
+		{
+			@this.IsReadOnly = true;
+			return @this;
+		}
+	}
+}
