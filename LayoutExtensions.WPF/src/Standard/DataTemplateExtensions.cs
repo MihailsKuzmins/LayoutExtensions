348f85ccbc9946621ab3b9d444545e99a7171c4c
mihails.kuzmins <mihails.kuzmins01@gmail.com>
2019-07-18 19:56:30 +0300

    update package info

--- LayoutExtensions.WPF/src/Standard/DataTemplateExtensions.cs
+++ LayoutExtensions.WPF/src/Standard/DataTemplateExtensions.cs
@@ -3,7 +3,7 @@ using System;
 using System.Windows;
 using System.Windows.Data;
 
-namespace LayoutExtensions.WPF.src.Standard
+namespace LayoutExtensions.WPF
 {
 	public static class DataTemplateExtensions
 	{
