4018130968d7ac3b53f01239848daf54fa801d46
mihails.kuzmins <mihails.kuzmins01@gmail.com>
2019-07-18 19:37:16 +0300

    ImageExtensions and StringExtensions

---
+++ LayoutExtensions.WPF/src/Standard/ImageExtensions.cs
@@ -0,0 +1,15 @@
+﻿using JetBrains.Annotations;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace LayoutExtensions.WPF
+{
+	public static class ImageExtensions
+	{
+		public static Image Source([NotNull] this Image @this, ImageSource imageSource)
+		{
+			@this.Source = imageSource;
+			return @this;
+		}
+	}
+}
---
+++ LayoutExtensions.WPF/src/StringExtensions.cs
@@ -0,0 +1,21 @@
+﻿using JetBrains.Annotations;
+using System;
+using System.IO;
+using System.Reflection;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace LayoutExtensions.WPF
+{
+	public static class StringExtensions
+	{
+		public static ImageSource ToImageSource([NotNull] this string @this, string iconNameWithExtension, Assembly assembly, params string[] pathSegments)
+		{
+			var assemblyName = assembly.FullName;
+			var resourcePath = Path.Combine(pathSegments);
+
+			return new BitmapImage(
+				new Uri($"pack://application:,,,/{assemblyName};component/{resourcePath}/{iconNameWithExtension}"));
+		}
+	}
+}
