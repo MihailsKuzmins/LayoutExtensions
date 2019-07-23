afdf64cf9cca11a56a654cf373db681ee953ef45
MihailsKuzmins <47413092+MihailsKuzmins@users.noreply.github.com>
2019-07-23 21:35:07 +0300

    Feature/key binding (#11)
    * fix spelling error
    
    * Update README.md
    
    * more extensions for KeyBinding
    
    * ComboBoxExtensions and make ItemsControlExtensions generic
    
    * deleted the incorrect file
    
    * Update README.md
    
    * updated assembly info

---
+++ LayoutExtensions.WPF/src/Generic/ItemsControlExtensions.cs
@@ -0,0 +1,48 @@
+﻿using JetBrains.Annotations;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace LayoutExtensions.WPF
+{
+	public static class ItemsControlExtensions
+	{
+		public static void GroupStyle<TItemsControl>([NotNull] this TItemsControl @this, DataTemplate headerTemplate)
+			where TItemsControl : ItemsControl
+		{
+			var groupStyle = new GroupStyle
+			{
+				HeaderTemplate = headerTemplate
+			};
+
+			@this.GroupStyle.Add(groupStyle);
+		}
+
+		public static TItemsControl ItemsPanel<TItemsControl>([NotNull] this TItemsControl @this, ItemsPanelTemplate itemsPanel)
+			where TItemsControl : ItemsControl
+		{
+			@this.ItemsPanel = itemsPanel;
+			return @this;
+		}
+
+		public static TItemsControl ItemTemplate<TItemsControl>([NotNull] this TItemsControl @this, DataTemplate itemTemplate)
+			where TItemsControl : ItemsControl
+		{
+			@this.ItemTemplate = itemTemplate;
+			return @this;
+		}
+
+		public static TItemsControl ItemContainerStyle<TItemsControl>([NotNull] this TItemsControl @this, Style itemContainerStyle)
+			where TItemsControl : ItemsControl
+		{
+			@this.ItemContainerStyle = itemContainerStyle;
+			return @this;
+		}
+
+		public static TItemsControl DisplayMemberPath<TItemsControl>([NotNull] this TItemsControl @this, string displayMemberPath)
+			where TItemsControl : ItemsControl
+		{
+			@this.DisplayMemberPath = displayMemberPath;
+			return @this;
+		}
+	}
+}
