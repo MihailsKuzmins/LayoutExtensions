514fd9f0c1d1d1ac24620f6b41c7590e7406fa1d
mihails.kuzmins <mihails.kuzmins01@gmail.com>
2019-07-23 21:26:13 +0300

    ComboBoxExtensions and make ItemsControlExtensions generic

---
+++ LayoutExtensions.WPF/src/Standard/ComboBoxExtensions.cs
@@ -0,0 +1,14 @@
+﻿using JetBrains.Annotations;
+using System.Windows.Controls;
+
+namespace LayoutExtensions.WPF
+{
+	public static class ComboBoxExtensions
+	{
+		public static ComboBox Editable([NotNull] this ComboBox @this)
+		{
+			@this.IsEditable = true;
+			return @this;
+		}
+	}
+}
