eb6e9da783467c04e3e598a871bd546c4c90bb34
mihails.kuzmins <mihails.kuzmins01@gmail.com>
2019-08-14 21:32:00 +0300

    created ButtonBaseExtensions

---
+++ LayoutExtensions.WPF/src/Generic/ButtonBaseExtensions.cs
@@ -0,0 +1,16 @@
+﻿using JetBrains.Annotations;
+using System.Windows.Controls.Primitives;
+using System.Windows.Input;
+
+namespace LayoutExtensions.WPF.src.Generic
+{
+	public static class ButtonBaseExtensions
+	{
+		public static TButtonBase Command<TButtonBase>([NotNull] this TButtonBase @this, ICommand command)
+			where TButtonBase : ButtonBase
+		{
+			@this.Command = command;
+			return @this;
+		}
+	}
+}
