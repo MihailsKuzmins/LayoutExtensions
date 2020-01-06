1c68853631e64e5b09ba6ece70af607fbfd4aac0
mihails.kuzmins <mihails.kuzmins@daytongroup.lv>
2020-01-06 13:59:54 +0200

    added binding extensions

---
+++ LayoutExtensions.WPF/src/Generic/BindingExtensions.cs
@@ -0,0 +1,42 @@
+﻿using System.Windows;
+using System.Windows.Data;
+
+namespace LayoutExtensions.WPF
+{
+	public static class BindingExtensions
+	{
+		public static TFrameworkElement SetBinding<TFrameworkElement>(this TFrameworkElement @this, DependencyProperty dp, string path)
+			where TFrameworkElement : FrameworkElement
+		{
+			@this.SetBinding(dp, path);
+			return @this;
+		}
+
+		public static TFrameworkElement SetBinding<TFrameworkElement>(this TFrameworkElement @this, DependencyProperty dp, Binding binding)
+			where TFrameworkElement : FrameworkElement
+		{
+			@this.SetBinding(dp, binding);
+			return @this;
+		}
+
+		public static TFrameworkElement SetBinding<TFrameworkElement>(this TFrameworkElement @this, DependencyProperty dp, string path, BindingMode bindingMode)
+			where TFrameworkElement : FrameworkElement
+		{
+			@this.SetBinding(dp, new Binding(path)
+			{
+				Mode = bindingMode
+			});
+			return @this;
+		}
+
+		public static TFrameworkElement SetBinding<TFrameworkElement>(this TFrameworkElement @this, DependencyProperty dp, string path, IValueConverter converter)
+			where TFrameworkElement : FrameworkElement
+		{
+			@this.SetBinding(dp, new Binding(path)
+			{
+				Converter = converter
+			});
+			return @this;
+		}
+	}
+}
