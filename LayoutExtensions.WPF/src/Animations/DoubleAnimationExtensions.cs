8d418d5acc82bb15e23e329f3649d3f1f7c59cce
mihails.kuzmins <mihails.kuzmins@daytongroup.lv>
2020-02-20 14:05:46 +0200

    added extensions for Timeline and DoubleAnimations

---
+++ LayoutExtensions.WPF/src/Animations/DoubleAnimationExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows.Media.Animation;
+
+// ReSharper disable once CheckNamespace
+namespace LayoutExtensions.WPF
+{
+	public static class DoubleAnimationExtensions
+	{
+		public static DoubleAnimation ToValue(this DoubleAnimation @this, double toValue)
+		{
+			@this.To = toValue;
+			return @this;
+		}
+	}
+}
---
+++ LayoutExtensions.WPF/src/Animations/TimelineExtensions.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Media.Animation;
+
+// ReSharper disable once CheckNamespace
+namespace LayoutExtensions.WPF
+{
+	public static class TimelineExtensions
+	{
+		public static TTimeline Duration<TTimeline>(this TTimeline @this, TimeSpan duration)
+			where TTimeline : Timeline
+		{
+			@this.Duration = duration.Duration();
+			return @this;
+		}
+
+		public static TTimeline RepeatBehaviour<TTimeline>(this TTimeline @this, RepeatBehavior repeatBehavior)
+			where TTimeline : Timeline
+		{
+			@this.RepeatBehavior = repeatBehavior;
+			return @this;
+		}
+
+		public static TTimeline TargetProperty<TTimeline>(this TTimeline @this, string propertyPath)
+			where TTimeline : Timeline
+		{
+			Storyboard.SetTargetProperty(@this, new PropertyPath(propertyPath));
+			return @this;
+		}
+
+		public static TTimeline AddToStoryboard<TTimeline>(this TTimeline @this, Storyboard storyboard)
+			where TTimeline : Timeline
+		{
+			storyboard.Children.Add(@this);
+			return @this;
+		}
+	}
+}
