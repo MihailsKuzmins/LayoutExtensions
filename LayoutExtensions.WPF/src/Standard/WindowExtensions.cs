13b67af1f5e7520cfaf8199f4e621fbdcbe81186
mihails.kuzmins <mihails.kuzmins@daytongroup.lv>
2020-02-20 10:16:39 +0200

    added window extensions

---
+++ LayoutExtensions.WPF/src/Standard/WindowExtensions.cs
@@ -0,0 +1,29 @@
+﻿using System.Windows;
+
+namespace LayoutExtensions.WPF
+{
+	public static class WindowExtensions
+	{
+		public static Window WindowStyle(this Window @this, WindowStyle windowStyle)
+		{
+			@this.WindowStyle = windowStyle;
+			return @this;
+		}
+
+		public static Window ResizeMode(this Window @this, ResizeMode resizeMode)
+		{
+			@this.ResizeMode = resizeMode;
+			return @this;
+		}
+
+		public static Window Bounds(this Window @this, double left, double top, double width, double height)
+		{
+			@this.Left = left;
+			@this.Top = top;
+			@this.Width = width;
+			@this.Height = height;
+
+			return @this;
+		}
+	}
+}
