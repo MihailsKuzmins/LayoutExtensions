cb014c8de052150df0a1b0863ad61f0bf5999660
mihails.kuzmins <mihails.kuzmins@daytongroup.lv>
2020-03-11 12:53:17 +0200

    added Description creator

--- utils/Wpf.MethodDescriptionGenerator/Program.cs
+++ utils/Wpf.MethodDescriptionGenerator/Program.cs
@@ -1,8 +1,8 @@
-﻿using System;
+﻿using LayoutExtensions.WPF.Controls;
+using Microsoft.Extensions.ObjectPool;
+using System;
 using System.Linq;
 using System.Reflection;
-using LayoutExtensions.WPF;
-using LayoutExtensions.WPF.Controls;
 
 namespace LayoutExtensions.Wpf.MethodDescriptionGenerator
 {
@@ -10,9 +10,48 @@ namespace LayoutExtensions.Wpf.MethodDescriptionGenerator
 	{
 		private static void Main()
 		{
-			var classes = typeof(DependencyObjectExtensions).Assembly
+			const string extensionsSuffix = "Extensions";
+
+			var stringBuilderPool = new DefaultObjectPoolProvider()
+				.CreateStringBuilderPool();
+
+			var extensionGroups = typeof(DependencyObjectExtensions).Assembly
 				.GetExportedTypes()
-				.Where(x => x.Name.EndsWith("Extensions"));
+				.Where(x => x.Name.EndsWith(extensionsSuffix))
+				.GroupBy(x =>
+				{
+					// ReSharper disable once PossibleNullReferenceException
+					var groupSeparatorIndex = x.Namespace.LastIndexOf('.');
+					return x.Namespace.Substring(groupSeparatorIndex + 1);
+				}, x => (x.Name, Funcs: x.GetMethods(BindingFlags.Public | BindingFlags.Static)))
+				.OrderBy(x => x.Key)
+				.Select(grouping =>
+				{
+					var classes = grouping
+						.Select(x =>
+						{
+							var funcNames = x.Funcs
+								.Select(y => y.Name)
+								.Distinct()
+								.OrderBy(y => y);
+
+							return stringBuilderPool.Get()
+								.AppendFormat(" - **{0}** - ", x.Name.Replace(extensionsSuffix, string.Empty))
+								.AppendJoin(", ", funcNames)
+								.ToString();
+						});
+
+					return stringBuilderPool.Get()
+						.AppendFormat("### {0} extensions:", grouping.Key)
+						.AppendLine()
+						.AppendJoin(';' + Environment.NewLine, classes)
+						.ToString();
+				});
+
+			var description = stringBuilderPool.Get()
+				.AppendJoin('.' + Environment.NewLine + Environment.NewLine, extensionGroups)
+				.Append('.')
+				.ToString();
 		}
 	}
 }
