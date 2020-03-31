5dfe55f041b434f9671542f81d4fe80769ccf75b
mihails.kuzmins <mihails.kuzmins@daytongroup.lv>
2020-03-31 18:27:54 +0300

    assign returns value

--- src/LayoutExtensions.WPF/src/Controls/DependencyObjectExtensions.cs
+++ src/LayoutExtensions.WPF/src/Controls/DependencyObjectExtensions.cs
@@ -4,10 +4,11 @@ namespace LayoutExtensions.WPF.Controls
 {
 	public static class DependencyObjectExtensions
 	{
-		public static void Assign<TDependencyObject>(this TDependencyObject @this, out TDependencyObject assignElement)
+		public static TDependencyObject Assign<TDependencyObject>(this TDependencyObject @this, out TDependencyObject assignElement)
 			where TDependencyObject : DependencyObject
 		{
 			assignElement = @this;
+			return @this;
 		}
 	}
 }
