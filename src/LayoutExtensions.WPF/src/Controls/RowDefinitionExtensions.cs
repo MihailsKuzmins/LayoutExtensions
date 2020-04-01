1edf5755eaa03282b9cf736f1edf25670824617b
mihails.kuzmins <mihails.kuzmins@daytongroup.lv>
2020-04-01 10:18:12 +0300

    added RowDefinitions and ColumnDefinitions

---
+++ src/LayoutExtensions.WPF/src/Controls/RowDefinitionExtensions.cs
@@ -0,0 +1,29 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace LayoutExtensions.WPF.Controls
+{
+	public static class RowDefinitionExtensions
+	{
+		public static TRowDefinition Auto<TRowDefinition>(this TRowDefinition @this, double value = 1d)
+			where TRowDefinition : RowDefinition
+		{
+			@this.Height = new GridLength(value, GridUnitType.Auto);
+			return @this;
+		}
+
+		public static TRowDefinition Pixel<TRowDefinition>(this TRowDefinition @this, double value = 1d)
+			where TRowDefinition : RowDefinition
+		{
+			@this.Height = new GridLength(value, GridUnitType.Pixel);
+			return @this;
+		}
+
+		public static TRowDefinition Star<TRowDefinition>(this TRowDefinition @this, double value = 1d)
+			where TRowDefinition : RowDefinition
+		{
+			@this.Height = new GridLength(value, GridUnitType.Star);
+			return @this;
+		}
+	}
+}
